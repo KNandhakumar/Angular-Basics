cd0e634c717a38311313dd5093f0d4dea9bb3092
Nandhakumar <nknandhakumar64@gmail.com>
2024-06-10 16:15:38 +0530

    Navbar

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        'golden-yellow': '#ffae00',
+        'main-bg-color': '#1f1f1f'
+      }
+    },
+  },
+  plugins: [],
+}
+
